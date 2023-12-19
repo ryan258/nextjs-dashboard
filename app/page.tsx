8f88ab283e08269cfb713a11fc0e62d9fba30dee
Ryan Johnson <ryanleejwebdev@gmail.com>
2023-12-19 15:27:12 -0600

    handle build error @ /app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the mother
-            lovin'{' '}
+            lovin{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
